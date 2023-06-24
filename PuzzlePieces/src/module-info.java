720f343fe51358154f30ee34fdb0f50d6078309b
Anpin5432 <134564455+Anpin5432@users.noreply.github.com>
2023-06-23 20:34:12 -0500

    Codigo del rompecabezas

---
+++ PuzzlePieces/src/module-info.java
@@ -0,0 +1,9 @@
+module PuzzlePieces {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires java.desktop;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
